30f0acb3b5728da5d2e6d81b07c377c04f5bd60b
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-02-05 00:21:04 +0700

    Update AccountDao.java

--- src/main/java/com/longnh02/techmate/Dao/AccountDao.java
+++ src/main/java/com/longnh02/techmate/Dao/AccountDao.java
@@ -4,34 +4,44 @@ import com.longnh02.techmate.Connection.ConnectionUtils;
 import com.longnh02.techmate.Models.Account;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountDao implements Dao<Account>{
 
-    Connection connection = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
+    private Connection connection = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
 
 
 
     @Override
     public void insert(Account account) {
-        int isCheck = 0;
         String query = "INSERT INTO account(username, password, account_typed, account_status) VALUES (?, ?, ?, ?)";
         try {
             connection = ConnectionUtils.getConnection();
             ps = connection.prepareStatement(query);
             ps.setString(1, account.getUsername());
             ps.setString(2, account.getPassword());
-            ps.setInt(3, account.getAccountDetailId());
-            ps.setInt(4, account.getRoleId());
-            ps.setInt(5, account.getStatus());
-            isCheck = ps.executeUpdate();
-            JDBCConnection.closeResultSet(rs);
-            JDBCConnection.closePreparedStatement(ps);
-            JDBCConnection.closeConnection(connection);
+            ps.setInt(3, account.getAccountTyped());
+            ps.setString(4, account.getAccountStatus());
+            ps.executeUpdate();
+            System.out.println("Data Added Successfully");
+
         } catch (SQLException e) {
             System.out.println(e);
+        } finally {
+            ConnectionUtils.closePreparedStatement(ps);
+            ConnectionUtils.closeConnection(connection);
         }
-        return isCheck > 0;
     }
+
+    @Override
+    public void update(Account account, int id) {
+    }
+
+    @Override
+    public void delete(Account account) {
+    }
+
 }
