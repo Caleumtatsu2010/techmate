9ac96d58f30f1ef87197c61ad4d712d8d0d2c766
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-02-04 21:40:33 +0700

    Create Supplier.java

---
+++ src/main/java/com/longnh02/techmate/Models/Supplier.java
@@ -0,0 +1,18 @@
+package com.longnh02.techmate.Models;
+
+public class Supplier {
+    private int id;
+    private String name;
+    private String desc;
+    private String emailAddress;
+    private int businessPhone;
+    private int mobilePhone;
+    private String address_line1;
+    private String address_line2;
+    private String town;
+    private String district;
+    private String city;
+    private String state_province;
+    private String country;
+    private String postalCode;
+}
