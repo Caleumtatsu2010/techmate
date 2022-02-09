fb79b74535504829074b532c5c35116c8f8f6fc4
Nguyen Hoang Long <shinyakogami2010@gmail.com>
2022-02-10 01:08:51 +0700

    update product

---
+++ src/main/java/com/longnh02/techmate/Admin/Controllers/IndexProduct.java
@@ -0,0 +1,32 @@
+package com.longnh02.techmate.Admin.Controllers;
+
+import com.longnh02.techmate.Dao.AccountDao;
+import com.longnh02.techmate.Dao.ProductDao;
+import com.longnh02.techmate.Models.Account;
+import com.longnh02.techmate.Models.Product;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "IndexProduct", value = "/IndexProduct")
+public class IndexProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        ProductDao prodDao = new ProductDao();
+
+        List<Product> products = prodDao.getAll();
+
+        request.setAttribute("listProduct", products);
+
+        request.getRequestDispatcher("IndexProduct.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
