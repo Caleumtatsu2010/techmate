b391002113e9793d2dbd87df919a07b90f56e00f
Nguyen Hoang Long <shinyakogami2010@gmail.com>
2022-02-10 13:30:49 +0700

    update quantity, color

--- src/main/java/com/longnh02/techmate/Admin/Controllers/DetailProduct.java
+++ src/main/java/com/longnh02/techmate/Admin/Controllers/DetailProduct.java
@@ -1,20 +1,45 @@
 package com.longnh02.techmate.Admin.Controllers;
 
+import com.longnh02.techmate.Dao.AccountDao;
+import com.longnh02.techmate.Dao.ProductColorDao;
+import com.longnh02.techmate.Dao.ProductDao;
+import com.longnh02.techmate.Models.Account;
+import com.longnh02.techmate.Models.Product;
+import com.longnh02.techmate.Models.ProductColor;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "DetailProduct", value = "/DetailProduct")
 public class DetailProduct extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
         int id =Integer.parseInt(request.getParameter("id"));
+
+        ProductDao prodDao = new ProductDao();
+        Product product = prodDao.get(id);
+        int quantity = prodDao.getQuantityById(id);
+
+        List<String> listcolor = prodDao.getColorsById(id);
+
+
+        request.setAttribute("Product", product);
+        request.setAttribute("ListColors", listcolor);
+        request.setAttribute("Quantity", quantity);
+
+        request.getRequestDispatcher("DetailProduct.jsp").forward(request, response);
+
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 }
--- src/main/java/com/longnh02/techmate/Dao/ProductDao.java
+++ src/main/java/com/longnh02/techmate/Dao/ProductDao.java
@@ -20,9 +20,76 @@ public class ProductDao implements Dao<Product>{
 
     @Override
     public Product get(int id) {
+        String query = "SELECT * FROM product WHERE id = ?";
+        try {
+            connection = ConnectionUtils.getConnection();
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Product product = new Product(rs.getInt("id"), rs.getString("name"), rs.getString("desc"), rs.getString("SKU"),rs.getDouble("price"),
+                        rs.getString("unit_price"), rs.getInt("category_id"), rs.getInt("discount_id"), rs.getInt("inventory_id"), rs.getInt("supplier_id"),
+                        rs.getString("product_short_desc"), rs.getString("product_long_desc"), rs.getBinaryStream("image"));
+                return product;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            ConnectionUtils.closePreparedStatement(ps);
+            ConnectionUtils.closeResultSet(rs);
+            ConnectionUtils.closeConnection(connection);
+        }
         return null;
     }
 
+    public List<String> getColorsById(int id) {
+
+        String query = "SELECT * FROM product_colors INNER JOIN product ON  product_colors.product_id = product.id  where product.id = ?";
+        List<String> list = new ArrayList<>();
+        try {
+            connection = ConnectionUtils.getConnection();
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(rs.getString("color"));
+            }
+            return list;
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            ConnectionUtils.closePreparedStatement(ps);
+            ConnectionUtils.closeResultSet(rs);
+            ConnectionUtils.closeConnection(connection);
+        }
+        return null;
+
+    }
+
+    public int getQuantityById(int id) {
+        String query = "SELECT * FROM product_inventory INNER JOIN product ON product_inventory.id = product.inventory_id where product.id = ?";//inventory_id in product
+        try {
+            connection = ConnectionUtils.getConnection();
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("quantity");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            ConnectionUtils.closePreparedStatement(ps);
+            ConnectionUtils.closeResultSet(rs);
+            ConnectionUtils.closeConnection(connection);
+        }
+        return 0;
+    }
     @Override
     public List<Product> getAll() {
         String query = "SELECT * FROM product";
