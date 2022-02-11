019294df2c62c5c2d37d887af0ea7e5fc1e15876
Nguyen Hoang Long <shinyakogami2010@gmail.com>
2022-02-11 08:54:12 +0700

    update

--- src/main/java/com/longnh02/techmate/Admin/Controllers/DetailProduct.java
+++ src/main/java/com/longnh02/techmate/Admin/Controllers/DetailProduct.java
@@ -1,17 +1,12 @@
 package com.longnh02.techmate.Admin.Controllers;
 
-import com.longnh02.techmate.Dao.AccountDao;
-import com.longnh02.techmate.Dao.ProductColorDao;
 import com.longnh02.techmate.Dao.ProductDao;
-import com.longnh02.techmate.Models.Account;
 import com.longnh02.techmate.Models.Product;
-import com.longnh02.techmate.Models.ProductColor;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "DetailProduct", value = "/DetailProduct")
