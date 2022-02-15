367c47db19a2fc432536dc1d8818fda0078faf0e
Nguyen Hoang Long <shinyakogami2010@gmail.com>
2022-02-15 16:33:26 +0700

    update

--- src/main/java/com/longnh02/techmate/Client/Controllers/AddToCart.java
+++ src/main/java/com/longnh02/techmate/Client/Controllers/AddToCart.java
@@ -5,8 +5,8 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Product", value = "/Product")
-public class Product extends HttpServlet {
+@WebServlet(name = "SAddTocart", value = "/SAddTocart")
+public class AddToCart extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
