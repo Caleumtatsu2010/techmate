ed514fdfaf3472313972f85195a50eb43522523d
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-10 19:42:38 +0700

    update

--- src/main/java/com/longnh02/techmate/admin/controllers/LoginAdmin.java
+++ src/main/java/com/longnh02/techmate/admin/controllers/LoginAdmin.java
@@ -1,7 +1,7 @@
 package com.longnh02.techmate.admin.controllers;
 
 import com.longnh02.techmate.dao.AccountDao;
-import com.longnh02.techmate.models.Account;
+import com.longnh02.techmate.models.account.Account;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
