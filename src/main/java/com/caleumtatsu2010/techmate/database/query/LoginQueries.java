7b830bb89a9953b5aab1ff60a98ae36ad6a137b9
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-11 16:58:00 +0700

    update

---
+++ src/main/java/com/caleumtatsu2010/techmate/database/query/LoginQueries.java
@@ -0,0 +1,6 @@
+package com.caleumtatsu2010.techmate.database.query;
+
+public class LoginQueries {
+
+    public static String login = "SELECT * FROM account WHERE username = ? and password = ?";
+}
