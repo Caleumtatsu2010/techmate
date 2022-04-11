e3b9a246dc2a2e03bca5d42be737616b5e4a1064
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-11 16:58:09 +0700

    update

--- src/main/java/com/caleumtatsu2010/techmate/admin/controllers/loginout/LogoutAdmin.java
+++ src/main/java/com/caleumtatsu2010/techmate/admin/controllers/loginout/LogoutAdmin.java
@@ -1,19 +1,18 @@
-package com.caleumtatsu2010.techmate.admin.controllers;
+package com.caleumtatsu2010.techmate.admin.controllers.loginout;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Logout", value = "/Logout")
-public class Logout extends HttpServlet {
+@WebServlet(name = "LogoutAdmin", value = "/LogoutAdmin")
+public class LogoutAdmin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.removeAttribute("account");
-
-            response.sendRedirect("Dashboard");
+            request.getRequestDispatcher("/LoginAdmin").forward(request, response);
         }
     }
 
