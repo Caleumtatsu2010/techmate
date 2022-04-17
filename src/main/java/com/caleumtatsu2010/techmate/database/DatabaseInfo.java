e6155788dd8d7b5dd849616b972f0df4466c06dc
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-17 22:26:09 +0700

    update

--- src/main/java/com/caleumtatsu2010/techmate/database/DatabaseInfo.java
+++ src/main/java/com/caleumtatsu2010/techmate/database/DatabaseInfo.java
@@ -2,13 +2,22 @@ package com.caleumtatsu2010.techmate.database;
 
 public class DatabaseInfo {
 
+    private static final String host = "localhost";
     private static final String dbname = "techmate";
     private static final String username = "root";
     private static final String password = "123456";
     private static final String port = "3306";
-    private static final String url = "jdbc:mysql://localhost:"+port+"/"+ dbname;
+    private static final String url = "jdbc:mysql://+" + host + "+:" + port + "/" + dbname;
 
     public DatabaseInfo() {
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public static String getPort() {
+        return port;
     }
 
     public static String getDbname() {
