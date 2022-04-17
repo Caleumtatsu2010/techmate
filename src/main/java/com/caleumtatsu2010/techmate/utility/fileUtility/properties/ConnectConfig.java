2a8623e21cdb1165bf7c6019e9558eddb9411de6
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-17 22:25:44 +0700

    update

---
+++ src/main/java/com/caleumtatsu2010/techmate/utility/fileUtility/properties/ConnectConfig.java
@@ -0,0 +1,46 @@
+package com.caleumtatsu2010.techmate.utility.fileUtility.properties;
+
+import com.caleumtatsu2010.techmate.models.properties.connection;
+
+import java.io.*;
+import java.util.Properties;
+
+public class ConnectConfig {
+    //connection information configuration file
+    private static File connectionFile = new File("D:\\stuff\\JAVA\\techmate\\src\\main\\java\\com\\caleumtatsu2010\\techmate\\properties\\connection.properties");
+
+    /**
+     *
+     * @param HOSTNAME
+     * @param DBNAME
+     * @param USERNAME
+     * @param PASSWORD
+     * @param PORTNAME
+     * @return connection read from properties file
+     */
+    public static connection readProperties(String HOSTNAME, String DBNAME, String USERNAME, String PASSWORD, String PORTNAME)
+    {
+        try {
+            FileReader reader = new FileReader(connectionFile);
+            Properties props = new Properties();
+            props.load(reader);
+
+            String host = props.getProperty(HOSTNAME, "localhost");
+            String dbname = props.getProperty(DBNAME);
+            String username = props.getProperty(USERNAME);
+            String password = props.getProperty(PASSWORD);
+            String port = props.getProperty(PORTNAME);
+
+            reader.close();
+            return new connection(host, dbname, username, password, port);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(readProperties("host", "dbname", "username", "password", "port").getUsername());
+    }
+}
+
