a32c761e5018891e518ced58f9116f1b5d853d28
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-04-17 22:25:49 +0700

    update

---
+++ src/main/java/com/caleumtatsu2010/techmate/models/properties/connection.java
@@ -0,0 +1,18 @@
+package com.caleumtatsu2010.techmate.models.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class connection {
+    private String host;
+    private String dbname;
+    private String username;
+    private String password;
+    private String port;
+}
