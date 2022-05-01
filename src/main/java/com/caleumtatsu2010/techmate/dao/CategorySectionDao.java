91235563d171c8c1ac38e36d554c2a563593f2e2
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-05-02 00:10:47 +0700

    update

--- src/main/java/com/caleumtatsu2010/techmate/dao/CategorySectionDao.java
+++ src/main/java/com/caleumtatsu2010/techmate/dao/CategorySectionDao.java
@@ -1,6 +1,6 @@
 package com.caleumtatsu2010.techmate.dao;
 
-import com.caleumtatsu2010.techmate.database.connection.ConnectionUtility;
+import com.caleumtatsu2010.techmate.database.connection.localdb.ConnectionUtility;
 import com.caleumtatsu2010.techmate.database.query.CategorySectionQueries;
 import com.caleumtatsu2010.techmate.models.category.CategorySection;
 
