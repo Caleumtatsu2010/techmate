799eb6cffd63e5e6884be8fb8492cf5164ae3cb3
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-05-03 15:43:28 +0700

    update

--- src/main/java/com/caleumtatsu2010/techmate/utility/file/properties/connection/Path.java
+++
@@ -1,9 +0,0 @@
-package com.caleumtatsu2010.techmate.utility.file.properties.connection;
-
-public class Path {
-    //project dir
-    private static String dir = System.getProperty("user.dir");
-    //properties connection infor file  path
-    public static String connectionFilePath = dir +"\\src\\main\\java\\com\\caleumtatsu2010\\techmate\\properties\\connection.properties";
-
-}
