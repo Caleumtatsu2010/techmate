66e1fdd62c444a0700d2b350ca823982d0b444f5
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2022-05-23 20:23:25 +0700

    update

---
+++ src/main/java/com/caleumtatsu2010/techmate/models/category/SubCategory.java
@@ -0,0 +1,17 @@
+package com.caleumtatsu2010.techmate.models.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubCategory {
+    private int id;
+    private String name;
+    private String desc;
+    private int productCategoryId;
+}
